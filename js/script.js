3c99352d0d3f5d7c27c93d67760384fba2cb2945
Juan Gonzalez <john256145@gmail.com>
2020-04-18 15:41:57 -0500

    completed lab

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
+  var input = document.getElementById('entryinput');
+  var results = document.getElementById('textoutput');
+  var button = document.getElementById('entrybutton');
+  button.addEventListener('click', function(){
+    results.innerHTML = '<h2>' + input.value + '</h2>';
+    alert("Juan Gonzalez: " + input.value);
+  });
+}
 window.addEventListener('load', init);
